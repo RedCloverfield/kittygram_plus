b09fb5874f995196dc58e5f3e642fdeb026906be
Станислав Ефимов <stanislav.karov@mail.ru>
2024-01-22 22:46:38 +0300

    Related models in Django Rest framework

--- cats/models.py
+++ cats/models.py
@@ -5,7 +5,17 @@ class Cat(models.Model):
     name = models.CharField(max_length=16)
     color = models.CharField(max_length=16)
     birth_year = models.IntegerField()
-
+    owner = models.ForeignKey(
+        'Owner', related_name='cats', on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return self.name
+
+
+class Owner(models.Model):
+    first_name = models.CharField(max_length=128)
+    second_name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return f'{self.first_name} {self.second_name}'
