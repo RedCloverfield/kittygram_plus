f13d69e9e506a0423811d2569ccaf9dfc0dd4509
Станислав Ефимов <stanislav.karov@mail.ru>
2024-01-24 17:08:54 +0300

    SerializerMethodField and custom field types creation in Django Rest framework

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,22 +1,46 @@
+import webcolors
 from rest_framework import serializers
+
+import datetime as dt
 
 from .models import Achievement, Cat, Owner, AchievementCat
 
 
+class Hex2NameColor(serializers.Field):
+
+    # При чтении данных ничего не меняем - просто возвращаем как есть
+    def to_representation(self, value):
+        return value
+
+    # При записи код цвета конвертируется в его название
+    def to_internal_value(self, data):
+        try:
+            # Если имя цвета существует, то конвертируем код в название
+            data = webcolors.hex_to_name(data)
+        except ValueError:
+            raise serializers.ValidationError('Для этого цвета нет имени!')
+        return data
+
+
 class AchievementSerializer(serializers.ModelSerializer):
+    achievement_name = serializers.CharField(source='name')
 
     class Meta:
         model = Achievement
-        fields = ('id', 'name')
+        fields = ('id', 'achievement_name')
 
 
 class CatSerializer(serializers.ModelSerializer):
+    # В ответе API вернется имя хоязина, а не его id
     # owner = serializers.StringRelatedField(read_only=True)
     achievements = AchievementSerializer(many=True, required=False)
+    age = serializers.SerializerMethodField()
+    # Цвет нужно будет передавать в hex-формате, возвращаться будет его название
+    # color = Hex2NameColor()
 
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements')
+        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements', 'age')
 
     def create(self, validated_data):
         # Если в исходном запросе не было поля achievements
@@ -38,6 +62,9 @@ class CatSerializer(serializers.ModelSerializer):
                 achievement=current_achievement, cat=cat
             )
         return cat
+
+    def get_age(self, obj):
+        return dt.datetime.now().year - obj.birth_year
 
 
 class OwnerSerializer(serializers.ModelSerializer):
